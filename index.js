b07f05011e5f56834be4ad7a6fac5215a692face
calmpsycho333 <wrathking75@yahoo.com>
2021-06-17 20:37:55 -0400

    commit tree ba797e80a09fa97e1ebc6cf31d09aa1af2794948

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue(drivers, key, value) {
+//this function should take in a driver Object, a key and a value. The function should not mutate the driver parameter and return
+ //a new driver that has an updated value for the key passed in.
+    
+
+    return Object.assign({}, drivers, { [key]: value });
+};
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+
+    driver[key] = value;
+
+    return driver;
+
+};
+
+function deleteFromDriverByKey(driver, key) {
+  
+      const NEW_OBJ =  Object.assign({}, driver);
+  delete NEW_OBJ[key];
+  return NEW_OBJ;
+};
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    Object.assign({}, driver);
+delete driver[key];
+return driver;
+}
